3e5232a6a4a132d03ccd9afe4232cde5affd54e7
Mikulas Vesely <mikface@gmail.com>
2022-12-09 23:01:01 -0500

    remove useless/deprecated dependencies, bump symfony, bump cdn77/coding-standard

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -6,7 +6,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
